67d8858ec9b2eaf97f92ed43124fd60d551a692f
61124 <onlyad.26@gmail.com>
2025-11-10 15:30:54 +0530

    Implement unique ID check and new student retrieval methods
    Added unique ID check for students and new retrieval functions.

--- student.sol
+++ student.sol
@@ -2,28 +2,35 @@
 pragma solidity ^0.8.0;
 
 contract StudentData {
-    // Structure to hold student details
     struct Student {
         uint id;
         string name;
         string course;
     }
 
-    // Array to store students
     Student[] public students;
 
-    // Function to add a new student
-    function addStudent(uint _id, string memory _name, string memory _course) public {
-        students.push(Student(_id, _name, _course));
+    // Track used IDs
+    mapping(uint => bool) private idExists;
+
+    // Modifier to ensure unique ID
+    modifier uniqueId(uint _id) {
+        require(!idExists[_id], "Student with this ID already exists");
+        _;
     }
 
-    // Function to get student details by index
+    // Add new student only if ID not used before
+    function addStudent(uint _id, string memory _name, string memory _course) public uniqueId(_id) {
+        students.push(Student(_id, _name, _course));
+        idExists[_id] = true;
+    }
+
     function getStudent(uint index) public view returns (uint, string memory, string memory) {
         require(index < students.length, "Student index out of range");
         Student memory student = students[index];
         return (student.id, student.name, student.course);
     }
-    
+
     function getStudentById(uint _id) public view returns (string memory, string memory) {
         for (uint i = 0; i < students.length; i++) {
             if (students[i].id == _id) {
@@ -33,19 +40,33 @@ contract StudentData {
         revert("Student not found");
     }
 
+    function getAllStudents() public view returns (Student[] memory) {
+        return students;
+    }
+
+    function getStudentsByIds(uint[] memory _ids) public view returns (Student[] memory) {
+        Student[] memory result = new Student[](_ids.length);
+        for (uint i = 0; i < _ids.length; i++) {
+            bool found = false;
+            for (uint j = 0; j < students.length; j++) {
+                if (students[j].id == _ids[i]) {
+                    result[i] = students[j];
+                    found = true;
+                    break;
+                }
+            }
+            require(found, "One or more IDs not found");
+        }
+        return result;
+    }
+
     function getTotalStudents() public view returns (uint) {
         return students.length;
     }
 
-    // Fallback function to handle incorrect function calls
     fallback() external payable {
         revert("Function does not exist!");
     }
 
-    // Function to receive Ether
-    receive() external payable {
-        // Custom logic for receiving Ether can be added here
-    }
-
-    
+    receive() external payable {}
 }
